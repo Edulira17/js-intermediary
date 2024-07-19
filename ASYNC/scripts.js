ba38a3b56b295ebe7895493a138db40dbbffd6b4
root <root@DESKTOP-PTEF590>
2024-07-19 09:58:07 -0400

    Iniciando Seção DOM

--- ASYNC/scripts.js
+++ ASYNC/scripts.js
@@ -53,4 +53,69 @@ function checkNumber(n){
 const a = checkNumber(20)
 const b = checkNumber(10)
 
-a.then((v) => console.log `O resultado é ${v}`).catch((err) => console.log(`Um erro ocorre: ${err}`))
+a.then((v) => console.log (`O resultado é ${v}`)).catch((err) => console.log(`Um erro ocorre: ${err}`))
+
+b.then((v) => console.log (`O resultado é ${v}`)).catch((err) => console.log(`Um erro ocorre: ${err}`))
+
+// Resolvendo várias promises
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(function(){
+        resolve(10)
+    }, 3000)
+})
+
+const p2 = Promise.resolve(10 + 10)
+
+const p3 = new Promise((resolve,reject) => {
+    if(30 > 10) {
+        resolve(30)
+    }else {
+        reject("Erro!")
+    }
+})
+
+Promise.all([p1, p2, p3]).then((values) => console.log(values))
+
+// Async Functions
+async function somarComDeley(a, b){
+
+
+    return a + b
+}
+
+somarComDeley(2, 4).then((value) => {
+    console.log(`O resultado da soma é: ${value}`)
+})
+
+console.log("teste async")
+
+// Assertions - Instrução await
+// serve para aguardar o resultado de uma async function
+function resolveComDeley(){
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve("Resolveu a Promise")
+        }, 2000)
+    })
+}
+
+async function chamandoAsync() {
+    console.log("Chamando a Promise, e esperando o resultado")
+    const result = await resolveComDeley();
+    console.log(`O resultado chegou: ${result}`)
+}
+
+chamandoAsync();
+
+// Generators = funcionam de forma semelhante as promises; Ações podem ser pausadas e continuadas depois; Novos operadores como: function* e yield
+function* generator(){
+    yield 1
+    yield 2
+    yield 3
+}
+
+const gen = generator();
+
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
